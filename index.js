4be03ead7ed3219473ad684550fe4fc44d0827fa
Youngpuffy <levimuturi2@gmail.com>
2025-03-25 16:08:58 +0300

    fetch-lab

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((response)=> response.json())
+  .then(renderBooks)
+  }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
